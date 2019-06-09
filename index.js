bca6142fd88e5fc0a9dbf6fc91cc9b47ff6d140c
Shelly Huang <shellyhuang81@gmail.com>
2019-06-09 12:11:44 -0400

    add broadcast bork

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
 const app = express()
-const socket-io = require('socket.io')
+const server = app.listen(3000, () => console.log('running on port 3000'))
 
-app.listen(3000, () => console.log('running on port 3000'))
+const io = require('socket.io')(server)
+
+io.on('connection', (socket) => {
+    socket.on('broadcast-bork', () => {
+        socket.broadcast.emit('bork')
+    })
+})
 
 
